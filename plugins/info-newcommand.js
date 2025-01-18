1669dcb17642327861f74cf430d8c2a0b8615f1a
Kevin22Mza <167890463+Kevin22Mza@users.noreply.github.com>
2025-01-17 23:49:09 -0300

    Actualizar info-newcommand.js

--- plugins/info-newcommand.js
+++ plugins/info-newcommand.js
@@ -1,14 +1,14 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) return conn.reply(m.chat, '🐉 Que comando quieres sugerir?', m)
-    if (text.length < 10) return conn.reply(m.chat, '☁️ La sugerencia debe ser mas de 10 character.', m)
-    if (text.length > 1000) return conn.reply(m.chat, '💨 Maximo de la sugerencia es de 1000 character.', m)
-    const teks = `🐲 Sugerencia de un nuevo comando del usuario *${nombre}*
+    if (!text) return conn.reply(m.chat, '🐕 Que comando quieres sugerir?', m)
+    if (text.length < 10) return conn.reply(m.chat, '🐾 La sugerencia debe ser mas de 10 character.', m)
+    if (text.length > 1000) return conn.reply(m.chat, '🐾 Maximo de la sugerencia es de 1000 character.', m)
+    const teks = `🐶 Sugerencia de un nuevo comando del usuario *${nombre}*
 
-🐉 Comando Sugerido:
+🐶 Comando Sugerido:
 > ${text}`
-    await conn.reply('5491166401905@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
+    await conn.reply('5492612721386@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
 
-    m.reply('☁️ La sugerencia se envió a mi propietario.')
+    m.reply('🐾 La sugerencia se envió a mi propietario.')
 }
 handler.help = ['newcommand']
 handler.tags = ['info']
