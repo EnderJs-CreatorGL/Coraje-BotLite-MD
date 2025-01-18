3da66d4d9fd407295519252ba29408cf88d4dafa
Kevin22Mza <167890463+Kevin22Mza@users.noreply.github.com>
2025-01-18 00:47:00 -0300

    Actualizar index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ break;
 });
 p.on('exit', (_, code) => {
 isRunning = false;
-console.error('☁️ Error:\n', code);
+console.error('🐾 Error:\n', code);
 process.exit();
 if (code === 0) return;
 watchFile(args[0], () => {
@@ -73,7 +73,7 @@ p.emit('message', line.trim());
 }
 process.on('warning', (warning) => {
 if (warning.name === 'MaxListenersExceededWarning') {
-console.warn('☁️ Se excedió el límite de Listeners en:');
+console.warn('🐾 Se excedió el límite de Listeners en:');
 console.warn(warning.stack);
 }
 });
