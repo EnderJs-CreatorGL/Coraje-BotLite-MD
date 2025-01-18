2f490eff2db39665a6f821b66d279ae1091d0961
Kevin22Mza <167890463+Kevin22Mza@users.noreply.github.com>
2025-01-17 23:39:42 -0300

    Actualizar info-autoresponder.js

--- plugins/info-autoresponder.js
+++ plugins/info-autoresponder.js
@@ -1,19 +1,19 @@
 let handler = async (m, { conn, text, usedPrefix, command, isOwner, isAdmin, isROwner }) => {
 if (!(isOwner || isAdmin || isROwner)) {
-conn.reply(m.chat, "🐲 Losiento no puedes personalizar el autoresponder en este grupo/chat.", m)
+conn.reply(m.chat, "🐕 Lo siento no puedes personalizar el autoresponder en este grupo/chat.", m)
 }
 const chatData = global.db.data.chats[m.chat]
 if (text) {
 if (chatData.sAutoresponder) return conn.reply(m.chat, `⚠️ *Ya hay un prompt en uso, si quieres configurar otro escribe: ${usedPrefix + command}, hazlo sin texto.*`, m)
 
 chatData.sAutoresponder = text
-conn.reply(m.chat, `🐉 *Configuración con éxito.*\n\n☁️ Si el autoresponder está desactivado activalo usando:\n> » ${usedPrefix}on autoresponder`, m)
+conn.reply(m.chat, `🐕 *Configuración con éxito.*\n\n🐾 Si el autoresponder está desactivado activalo usando:\n> » ${usedPrefix}on autoresponder`, m)
 } else {
 if (chatData.sAutoresponder) {
 chatData.sAutoresponder = ''
 conn.reply(m.chat, "🗑️ *Prompt borrado con éxito.*", m, fake)
 } else {
-conn.reply(m.chat, `⚠️ *No hay Prompt personalizado en este chat.*\n\n💨 Puedes perzonalizar el autoresponder usando:\n> » ${usedPrefix + command} + texto que quieres que lo interactúe.`, m)
+conn.reply(m.chat, `⚠️ *No hay Prompt personalizado en este chat.*\n\n🐾 Puedes perzonalizar el autoresponder usando:\n> » ${usedPrefix + command} + texto que quieres que lo interactúe.`, m)
 }}
 }
 
