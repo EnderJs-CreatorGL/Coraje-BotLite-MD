3ab6701deb97e5d4bb2ae3f6b3de75b428ab0c2b
Kevin22Mza <167890463+Kevin22Mza@users.noreply.github.com>
2025-01-18 01:05:19 -0300

    Actualizar owner-dsowner.js

--- plugins/owner-dsowner.js
+++ plugins/owner-dsowner.js
@@ -31,7 +31,7 @@ await conn.reply(m.chat, '✨ *La carpeta escrita esta vacía*',  m, rcanal, )
 } else {
 m.react(done)
 await conn.reply(m.chat, `🌳 *Ejcutado ${filesDeleted} archivos fueron borrados de Coraje-BotLite-MD,excepto las creds.json*`,  m, rcanal, )
-conn.reply(m.chat, `🌲 *¡Hola! soy coraje-BotLite-MD ¿logras leerme?*`, m, rcanal, )
+conn.reply(m.chat, `🌲 *¡Hola! soy Coraje-BotLite-MD ¿logras leerme?*`, m, rcanal, )
 
 }
 } catch (err) {
