de1c32a702fb04f62f05dec13880c93813c012db
Kevin22Mza <167890463+Kevin22Mza@users.noreply.github.com>
2025-01-18 00:24:58 -0300

    Actualizar rpg-heal.js

--- plugins/rpg-heal.js
+++ plugins/rpg-heal.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
         return conn.reply(m.chat, '👤 El usuario no se encuentra en la base de Datos.', m);
     }
     if (user.dragones < 20) {
-        return conn.reply(m.chat, '💔 No tienes suficientes dragones para curarte. Necesitas al menos 50 dragones.', m);
+        return conn.reply(m.chat, '💔 No tienes suficientes perrunos para curarte. Necesitas al menos 50 perrunos.', m);
     }
     let healAmount = 40; 
     user.health += healAmount;
@@ -13,7 +13,7 @@ let handler = async (m, { conn }) => {
         user.health = 100; 
     }
     user.lastHeal = new Date();
-    let info = `❤️ *Te has curado ${healAmount} puntos de salud.*\n🐉 *dragones restantes:* ${user.dragones}\n❤️ *Salud actual:* ${user.health}`;
+    let info = `❤️ *Te has curado ${healAmount} puntos de salud.*\n🐕 *perrunos restantes:* ${user.dragones}\n❤️ *Salud actual:* ${user.health}`;
     await conn.sendMessage(m.chat, { text: info }, { quoted: m });
 };
 
