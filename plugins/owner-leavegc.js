0cdb8117fad671a28ee05aaff0571612cff5e8b8
MRSHAD0W666 <srperruno13@gmail.com>
2025-01-17 22:52:22 -0600

    Actualizar owner-leavegc.js

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -2,8 +2,7 @@ let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
 let chat = global.db.data.chats[m.chat]
 chat.welcome = false
-await conn.reply(id, `🐕 
-**¡¿De verdad esperan que enfrente esto?! ¡Que se vayan todos al carajo! Yo no nací para esto, ¡me largo antes de que esa cosa me atrape!"* 🐾💥
+await conn.reply(id, `🐕 *¡¿De verdad esperan que enfrente esto?! ¡Que se vayan todos al carajo! Yo no nací para esto, ¡me largo antes de que esa cosa me atrape!"* 🐾💥
 `) 
 await conn.groupLeave(id)
 try {  
