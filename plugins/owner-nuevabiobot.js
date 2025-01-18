a395e9467e7c14b7dd373453beee2bbff4007cbd
Kevin22Mza <167890463+Kevin22Mza@users.noreply.github.com>
2025-01-18 00:15:04 -0300

    Actualizar owner-nuevabiobot.js

--- plugins/owner-nuevabiobot.js
+++ plugins/owner-nuevabiobot.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text }) => {
-   if (!text) return conn.reply(m.chat, '☁️ *¡Te faltó el texto mi propietario!*', m, rcanal)
+   if (!text) return conn.reply(m.chat, '🐾 *¡Te faltó el texto mi propietario!*', m, rcanal)
      try {
                 await conn.updateProfileStatus(text).catch(_ => _)
                 conn.reply(m.chat, `✅️ Info Cambiada Con Exito!`, m, rcanal)
