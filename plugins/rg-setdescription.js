f283706a5acb34e022668570ba853710cd1bafa4
Kevin22Mza <167890463+Kevin22Mza@users.noreply.github.com>
2025-01-18 00:22:54 -0300

    Actualizar rg-setdescription.js

--- plugins/rg-setdescription.js
+++ plugins/rg-setdescription.js
@@ -6,10 +6,10 @@ const handler = async (m, { conn, command, usedPrefix, text }) => {
 let user = global.db.data.users[m.sender];
 
 if (user.description) {
-return conn.reply(m.chat, `💨 Ya tienes una descripción establecida, si quieres borrar la descripcion actual usa:\n> » ${usedPrefix}deldescription`, m);
+return conn.reply(m.chat, `🐾 Ya tienes una descripción establecida, si quieres borrar la descripcion actual usa:\n> » ${usedPrefix}deldescription`, m);
 }
 
-if (!text) return conn.reply(m.chat, `🐉︎ Debes especificar una descripcion valida para tu perfil.\n\n> ✐ Ejemplo » *${usedPrefix + command} Hola, uso WhatsApp!*`, m);
+if (!text) return conn.reply(m.chat, `🐕 Debes especificar una descripcion valida para tu perfil.\n\n> ✐ Ejemplo » *${usedPrefix + command} Hola, uso WhatsApp!*`, m);
 
 user.description = text;
 
