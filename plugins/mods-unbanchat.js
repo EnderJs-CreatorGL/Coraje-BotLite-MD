8ff77e71b877e2b5fb56bf992448698e9b90d512
Kevin22Mza <167890463+Kevin22Mza@users.noreply.github.com>
2025-01-18 00:04:53 -0300

    Actualizar mods-unbanchat.js

--- plugins/mods-unbanchat.js
+++ plugins/mods-unbanchat.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
 if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '🔴 *¡ESTE CHAT NO ESTÁ REGISTRADO!*', m, fake)
 let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return conn.reply(m.chat, '⭕ *¡KAKAROTO-BOT-MD NO ESTÁ BANEADO EN ESTE CHAT!*', m, fake)
+if (!chat.isBanned) return conn.reply(m.chat, '⭕ *¡Coraje-BotLite-MD NO ESTÁ BANEADO EN ESTE CHAT!*', m, fake)
 chat.isBanned = false
-await conn.reply(m.chat, '✅ *¡KAKAROTO-BOT-MD YA FUÉ DESBANEADO EN ESTE CHAT!*', m, fake)
+await conn.reply(m.chat, '✅ *¡Coraje-BotLite-MD YA FUÉ DESBANEADO EN ESTE CHAT!*', m, fake)
 }
 handler.help = ['unbanchat'];
 handler.tags = ['mods'];
