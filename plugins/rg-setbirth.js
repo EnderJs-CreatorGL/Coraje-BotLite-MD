7b0dd0a3fe088d1e8ef9865b03efc18f5681ee81
Kevin22Mza <167890463+Kevin22Mza@users.noreply.github.com>
2025-01-18 00:22:31 -0300

    Actualizar rg-setbirth.js

--- plugins/rg-setbirth.js
+++ plugins/rg-setbirth.js
@@ -6,10 +6,10 @@ const handler = async (m, { conn, command, usedPrefix, text }) => {
 let user = global.db.data.users[m.sender];
 
 if (user.birth) {
-return conn.reply(m.chat, `🐉 Ya tienes una fecha establecida, si quieres borrar la fecha actual usa:\n> » ${usedPrefix}delbirth`, m);
+return conn.reply(m.chat, `🐕 Ya tienes una fecha establecida, si quieres borrar la fecha actual usa:\n> » ${usedPrefix}delbirth`, m);
 }
 
-if (!text) return conn.reply(m.chat, `☁️ Debes ingresar una fecha válida para tu cumpleaños.\n\n> ✐ Ejemplo » *${usedPrefix + command} 01/01/2000* (dia/mes/año)`, m);
+if (!text) return conn.reply(m.chat, `🐾 Debes ingresar una fecha válida para tu cumpleaños.\n\n> ✐ Ejemplo » *${usedPrefix + command} 01/01/2000* (dia/mes/año)`, m);
 
 function validarFechaNacimiento(text) {
 const opcionesFecha = [
@@ -29,7 +29,7 @@ return text;
 
 let birth = validarFechaNacimiento(text);
 if (!birth) {
-return conn.reply(m.chat, `☁️ Recuerda elegir una fecha de nacimiento válida.\n> Ejemplo: ${usedPrefix + command} 01/12/2024`, m);
+return conn.reply(m.chat, `🐾 Recuerda elegir una fecha de nacimiento válida.\n> Ejemplo: ${usedPrefix + command} 01/12/2024`, m);
 }
 
 user.birth = birth;
