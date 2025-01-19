96f0a3dc7ec396e082d9c62c5c1f85d824ab7030
Kevin22Mza <167890463+Kevin22Mza@users.noreply.github.com>
2025-01-19 17:13:34 -0300

    Actualizar ai-ia.js

--- plugins/ai-ia.js
+++ plugins/ai-ia.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text }) => {
-if (!text) return m.reply('🐉 *Ingresa un texto para hablar con la Bot*')
+if (!text) return m.reply('🐕 *Ingresa un texto para hablar con la Bot*')
 
 try {
 let api = await fetch(`https://deliriussapi-oficial.vercel.app/ia/chatgpt?q=${text}`)
