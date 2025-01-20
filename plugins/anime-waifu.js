fe9aa060809d7b17f4bdee44ec7e487e16e916b4
Kevin22Mza <167890463+Kevin22Mza@users.noreply.github.com>
2025-01-20 11:20:17 -0300

    Actualizar anime-waifu.js

--- plugins/anime-waifu.js
+++ plugins/anime-waifu.js
@@ -13,7 +13,7 @@ let res = await fetch('https://api.waifu.pics/sfw/waifu')
 if (!res.ok) return
 let json = await res.json()
 if (!json.url) return 
-await conn.sendFile(m.chat, json.url, 'thumbnail.jpg', '☁️ *W A I F U* ☁️', fkontak, null, rcanal)
+await conn.sendFile(m.chat, json.url, 'thumbnail.jpg', '🐾 *W A I F U* 🐾', fkontak, null, rcanal)
 } catch {
 }}
 handler.help = ['waifu']
