aa8044e091b614266fe36b890892d9d4129fda59
Kevin22Mza <167890463+Kevin22Mza@users.noreply.github.com>
2025-01-20 11:18:48 -0300

    Actualizar anime-imagenes.js

--- plugins/anime-imagenes.js
+++ plugins/anime-imagenes.js
@@ -4,7 +4,7 @@ const res = (await axios.get(`https://raw.githubusercontent.com/Diomar-s/Kakarot
 const haha = await res[Math.floor(res.length * Math.random())];
 const messages = [['Imagen 1', dev, await res[Math.floor(res.length * Math.random())],
 [[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res[Math.floor(res.length * Math.random())], [[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res[Math.floor(res.length * Math.random())], [[]], [[]], [[]], [[]]], ['Imagen 4', dev, await res[Math.floor(res.length * Math.random())], [[]], [[]], [[]], [[]]]]
-await conn.sendCarousel(m.chat, '🐉 Resultado de ' + command, '🔎 Anime - ' + command, null, messages, m);
+await conn.sendCarousel(m.chat, '🐕 Resultado de ' + command, '🔎 Anime - ' + command, null, messages, m);
 };
 handler.command = handler.help = ['akira', 'akiyama', 'anna', 'asuna', 'ayuzawa', 'boruto', 'chiho', 'chitoge', 'deidara', 'erza', 'elaina', 'eba', 'emilia', 'hestia', 'hinata', 'inori', 'isuzu', 'itachi', 'itori', 'kaga', 'kagura', 'kaori', 'keneki', 'kotori', 'kurumi', 'madara', 'mikasa', 'miku', 'minato', 'naruto', 'nezuko', 'sagiri', 'sasuke', 'sakura', 'cosplay'];
 handler.tags = ['anime'];
