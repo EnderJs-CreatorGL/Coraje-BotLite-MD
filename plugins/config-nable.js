7f099a4cc295ea40a74065c1fbefe142cc2273a0
Kevin22Mza <167890463+Kevin22Mza@users.noreply.github.com>
2025-01-20 11:33:31 -0300

    Actualizar config-nable.js

--- plugins/config-nable.js
+++ plugins/config-nable.js
@@ -2,7 +2,7 @@ const handler = async (m, {conn, usedPrefix, command, args, isOwner, isAdmin, is
 
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
-const miniopcion = `☁️ *OPCIONES PARA GRUPOS* ☁️
+const miniopcion = `🐾 *OPCIONES PARA GRUPOS* 🐾
 
 ${usedPrefix + command} welcome
 ${usedPrefix + command} autoresponder
@@ -22,7 +22,7 @@ ${usedPrefix + command} modoadmin
 ${usedPrefix + command} antifake
 ${usedPrefix + command} antibot
 
-🐉 *OPCIONES PARA MI PROPIETARIO*
+🐕 *OPCIONES PARA MI PROPIETARIO*
 
 ${usedPrefix + command} public
 ${usedPrefix + command} status
